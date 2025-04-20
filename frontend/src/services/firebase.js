53633eb4344aa5565d8f6f615cce3a323b05d93f
Anurag <anuragchaudhuri3007@gmail.com>
2025-04-20 12:31:59 +0530

    Add: Configured Firebase

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -8,7 +8,7 @@ import {
 
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBymVqZDXvMqF_iTVoWCJJekkGsNr6oDFc",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
